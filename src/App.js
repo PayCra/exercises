a9a525f632facce0aaf1fb1afa0202042b571b56
paula <paula.krupa@gmail.com>
2020-01-30 14:53:32 +0100

    2.4. Printed guys, class components, styled.

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import Hello from './Components/Hello';
 
 function App() {
     return (
+        <body className="Background">
         <div className="App">
-            <Hello name="Zayn"/>
-            <Hello name="Harry"/>
-            <Hello name="Louis"/>
-            <Hello name="Liam"/>
-            <span>
-         <Hello/><span className="Ni">Ni</span><span className="a">a</span><span className="ll">ll</span>
-        </span>
+            <Hello name="Harry" mic="Harrys"/>
+            <Hello name="Liam" mic="Liams"/>
+            <Hello name="Zayn" mic="Zayns"/>
+            <Hello name="Louis" mic="Louiss"/>
+            <div><Hello/><span className="Ni">Ni</span><span className="a">a</span><span className="ll">ll</span></div>
         </div>
+        </body>
     );
 }
 
