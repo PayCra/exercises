b2ae52351b5c3795cca3f2ce542015c92930ebec
paula <paula.krupa@gmail.com>
2020-01-30 03:05:56 +0100

    1.3. Square Direction.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+      <div className="Posing">
+        <div className="Zayn square"/>
+        <div>
+          <div className="NiallGreen flag"/>
+          <div className="NiallWhite flag"/>
+          <div className="NiallOrange flag"/>
+        </div>
+        <div className="Harry square"/>
+        <div className="Louis square"/>
+        <div className="Liam square"/>
+      </div>
   );
 }
 
