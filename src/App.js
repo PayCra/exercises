dbcbdc42bf00f0ca58fd85b467dc9deb01d3d97f
paula <paula.krupa@gmail.com>
2020-01-30 13:25:03 +0100

    2.1. Printed guys, functional, greeting.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
+import Hello from './Components/Hello';
 
 function App() {
   return (
-    <div className="App">
-    </div>
-  );
+      <div className="App">
+        <Hello name="Zayn"/>
+        <Hello name="Harry"/>
+        <Hello name="Niall"/>
+        <Hello name="Liam"/>
+        <Hello name="Louis"/>
+      </div>
+  )
 }
 
 export default App;
