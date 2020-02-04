921f026101fc8c7a8b0fb15db06002d5089e0b78
paula <paula.krupa@gmail.com>
2020-02-04 16:12:34 +0100

    2.2_alt.1. Hello, guys formatted.

--- src/App.js
+++ src/App.js
@@ -10,8 +10,11 @@ function App() {
             <Hello name="Louis" mic="Louiss"/>
             <Hello name="Liam" mic ="Liams"/>
             <span>
-        <Hello/><span className="Ni">Ni</span><span className="a">a</span><span className="ll">ll</span>
-       </span>
+                <Hello/>
+                <span className="Ni">Ni</span>
+                <span className="a">a</span>
+                <span className="ll">ll</span>
+            </span>
         </div>
     );
 }
