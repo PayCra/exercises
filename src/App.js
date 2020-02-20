8f953292a9561ff0e7ee74516c2ba74c092ce693
paula <paula.krupa@gmail.com>
2020-02-20 16:32:55 +0100

    3.1. Christmas trees.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+
+  let levels;
+  let printedLevels = [];
+
+  for (levels=1; levels<21; levels++) {
+    let spaces = (20 - levels)/2;
+    console.log(' '.repeat(spaces) + 'O' + 'H'.repeat(levels) + 'O');
+    printedLevels.push(<div className="App" key={levels}>O{'H'.repeat(levels)}O</div>);
+  }
+
+  return printedLevels;
 }
 
 export default App;
