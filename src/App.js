3acfabe183e232bd14ebd54ca1410b16e5224337
paula <paula.krupa@gmail.com>
2020-02-20 16:38:04 +0100

    3.2. Square Direction toggles.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import './App.css';
+import ShadyBox from './Components/ShadyBox';
 
 function App() {
-  return (
-      <div className="Posing">
-        <div className="Zayn square"/>
-        <div>
-          <div className="NiallGreen flag"/>
-          <div className="NiallWhite flag"/>
-          <div className="NiallOrange flag"/>
+    return (
+        <div className="App">
+            <ShadyBox name="Louis"/>
+            <ShadyBox name="Zayn"/>
+            <ShadyBox name="Harry"/>
+            <ShadyBox name="Liam"/>
+            <ShadyBox name="Niall"/>
         </div>
-        <div className="Harry square"/>
-        <div className="Louis square"/>
-        <div className="Liam square"/>
-      </div>
-  );
+    );
 }
 
 export default App;
