51c8dc1c231345807af5036844253f3ceff1c6f3
paula <paula.krupa@gmail.com>
2020-02-21 14:19:48 +0100

    1.2_alt. Harry centered with auto.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="HelloToHarry">
-      Hello, Harry.
-    </div>
-  );
+    return (
+        <div className="middle">
+            <div className="HelloToHarry">
+                Hello, Harry.
+            </div>
+        </div>
+    );
 }
 
 export default App;
