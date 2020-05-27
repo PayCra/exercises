6a34f34ec622422e7b4b3e91c20faa96e4485433
paula <paula.krupa@gmail.com>
2020-05-27 10:08:15 +0200

    4.1. Diamonds.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,26 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+  let levels;
+  let PrintedLevels = [];
+
+  for (levels=1; levels<11; levels++) {
+    console.log(" ".repeat(10-levels) + "/".repeat(levels) + "\\".repeat(levels));
+    document.write("<div>" + "&nbsp;".repeat(10-levels)+"/".repeat(levels) + "\\".repeat(levels) + "</div>");
+    PrintedLevels.push(<div>{"/".repeat(levels) + "\\".repeat(levels)}</div>);
+  }
+
+  for (levels=10; levels>0; levels--) {
+    console.log(" ".repeat(10-levels) + "\\".repeat(levels) + "/".repeat(levels));
+    document.write("<div>" + "&nbsp;".repeat(10-levels)+"\\".repeat(levels) + "/".repeat(levels) + "</div>");
+    PrintedLevels.push(<div>{"\\".repeat(levels) + "/".repeat(levels)}</div>);
+  }
+
   return (
-    <div className="App">
-    </div>
+      <div className="App">
+        {PrintedLevels}
+      </div>
   );
 }
 
