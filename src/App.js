7f7c513a1b3d14b911bb6ee7e9328fb390a5a1c6
paula <paula.krupa@gmail.com>
2020-05-29 17:01:06 +0200

    5.2. Color changes on click.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    color: ""
+  };
+
+  handleClick = () => {
+    if (this.state.color==="") {
+      this.setState({color: "bluer"})
+    } else {
+      this.setState({color: ""})
+    }
+  };
+
+  render() {
+    return (
+        <div className="App">
+          <div className="Button" onClick={this.handleClick}>Click me</div>
+          <div className={"Circle " + this.state.color}>I am a pretty circle</div>
+        </div>
+    );
+  }
 }
 
 export default App;
