d0a7e2371a0d7d6303a0bcb6a42b9a19f233c48c
paula <paula.krupa@gmail.com>
2020-05-29 17:06:29 +0200

    5.2_alt. Change color on click with svg crown.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import Crown from './Components/Crown';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    color: ""
+  };
+
+  handleClick = () => {
+    if (this.state.color==="") {
+      this.setState({color: "bluer"})
+    } else {
+      this.setState({color: ""})
+    }
+  };
+
+  render() {
+    return (
+        <div className="App">
+          <div className="onhead">
+            <Crown/>
+            <div className="Button" onClick={this.handleClick}>Click me</div>
+          </div>
+          <div className={"Circle " + this.state.color}>I am a pretty circle</div>
+        </div>
+    );
+  }
 }
 
 export default App;
