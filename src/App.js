ae4ee6cb16e4e5ffe2ff2f9a81edfddb2cb7c6b1
paula <paula.krupa@gmail.com>
2020-05-29 16:57:08 +0200

    5.1. Alphabet.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,20 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+  let i;
+  let alphabet = [];
+
+  for (i=65;i<=90;i++) {
+    console.log(String.fromCharCode(i));
+    document.write(String.fromCharCode(i));
+    alphabet.push(<span>{String.fromCharCode(i)}</span>)
+  }
+
   return (
-    <div className="App">
-    </div>
+      <div className="App">
+        {alphabet}
+      </div>
   );
 }
 
