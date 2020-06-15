15d91f5dace3b25919ee9dc5c7f5c2697bb20369
paula <paula.krupa@gmail.com>
2020-06-15 17:19:01 +0200

    5.4. Counting phrases in text.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,29 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+  let textToGoThrough = prompt("Give me the text to search through:");
+
+  let textToFind = prompt("Give me the phrase you want to count:");
+
+  let foundAtPosition = 0;
+
+  let count = 0;
+
+  while (foundAtPosition !== -1) {
+    foundAtPosition = textToGoThrough.indexOf(textToFind,foundAtPosition);
+    if (foundAtPosition !== -1) {
+      count++;
+      foundAtPosition++;
+    }
+  }
+
   return (
-    <div className="App">
-    </div>
+      <div className="App">
+        You wrote: "{textToGoThrough}"
+        {<br/>}
+        The number of times "{textToFind}" appears in this text is: {count}.
+      </div>
   );
 }
 
