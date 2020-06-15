0a9d5ec07a659349a9ec97c76d7eb08e34896dad
paula <paula.krupa@gmail.com>
2020-06-15 17:26:18 +0200

    5.5. Louis gives me feels in arrays.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,29 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+  let days = [1, 2, 3, 4, 5, 6, 7];
+  let indexOfDays;
+  let myArray = [];
+  let arrayOfVerbalFeels = ["many","too many","so many"];
+
+  for (indexOfDays in days) {
+    if (days[indexOfDays]%2===1) {
+      myArray.push(
+          <div>{`For the ${days[indexOfDays]} day of Christmas Lou gave me ${days[indexOfDays] * 2} feels.`}</div>)
+    } else {
+      myArray.push(
+          <div>
+            {`For the ${days[indexOfDays]} day of Christmas Lou gave me ${arrayOfVerbalFeels[days[indexOfDays]/2-1]} feels.`}
+          </div>
+      )
+    }
+  }
+
   return (
-    <div className="App">
-    </div>
+      <div className="App">
+        {myArray}
+      </div>
   );
 }
 
