702ff17f49d3c377b19737b00038cb22f1d69b89
paula <paula.krupa@gmail.com>
2020-06-15 17:17:16 +0200

    5.3. Times tables.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import './App.css';
 
 function App() {
+
+  function myTimesTable(mynumber,start,end) {
+    let loopCounter = start;
+    for (loopCounter;loopCounter<=end;loopCounter++) {
+      document.write(` ${mynumber} * ${loopCounter} = ${mynumber*loopCounter} <br>`)
+    }
+  }
+
+  let usersWishes = prompt("Gimme a number that's not -1 to see some times tables:");
+
+  while (isNaN(usersWishes) || usersWishes === " " || usersWishes === "") {
+    usersWishes = prompt("That was not a number, dude. Gimme something to work with:");
+  }
+
+  myTimesTable(usersWishes,7,10);
+
   return (
-    <div className="App">
-    </div>
+      <div/>
   );
 }
 
