2bba0887fbf149da39dfc2a9a00d240a26d60ae8
paula <paula.krupa@gmail.com>
2020-06-16 17:57:37 +0200

    6.2_alt. Asking for a dog name with forEach.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import React from 'react';
-import './App.css';
 
 function App() {
+
+  let dogName = prompt("Propose a good dog name, please:");
+
+  let letters = [];
+
+  let dogNameIndex;
+
+  let isok = [];
+
+  for (dogNameIndex = 0; dogNameIndex <= dogName.length - 1; dogNameIndex++) {
+    letters.push(dogName.charAt(dogNameIndex));
+  }
+
+  function check(value, index, array) {
+    if (
+        (array[index].charCodeAt(0) >= 65 && array[index].charCodeAt(0) <= 90)
+        ||
+        (array[index].charCodeAt(0) >= 97 && array[index].charCodeAt(0) <= 122)
+    ) {
+      isok.push("1");
+    } else {
+      isok.push("0");
+    }
+  }
+
+  letters.forEach(check);
+
+  if (isok.indexOf("0") !== -1) {
+    document.write("Gimme big or small letters only, please.")
+  } else {
+    document.write(`${dogName}? Good name! Thanks.`)
+  }
+
   return (
-    <div className="App">
-    </div>
+      <div/>
   );
 }
 
