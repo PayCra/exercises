e20e01257c385031237760ad8cb24f4d25db1545
paula <paula.krupa@gmail.com>
2020-06-16 18:09:37 +0200

    6.2_alt_alt. Asking for a dog name with every.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React from 'react';
-import './App.css';
 
 function App() {
+
+  let dogName = prompt("Propose a good dog name, please:");
+
+  while (dogName === "") {
+    dogName = prompt("Propose a good dog name, please:");
+  }
+
+  let letters = [];
+
+  let dogNameIndex;
+
+  for (dogNameIndex=0; dogNameIndex<=dogName.length-1;dogNameIndex++) {
+    letters.push(dogName.charAt(dogNameIndex));
+  }
+
+  function check(value) {
+    return (value.charCodeAt(0) >= 65 && value.charCodeAt(0) <= 90)
+        ||
+        (value.charCodeAt(0) >= 97 && value.charCodeAt(0) <= 122);
+  }
+
+  let result = letters.every(check);
+
+  if (result === false) {
+    document.write("Gimme big or small letters only, please.")
+  } else {
+    document.write(`${dogName}? Good name! Thanks.`)
+  }
+
   return (
-    <div className="App">
-    </div>
+      <div/>
   );
 }
 
