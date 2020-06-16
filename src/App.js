ff70851c05f7aae0b8fafcf07a5796fcca0391c1
paula <paula.krupa@gmail.com>
2020-06-16 17:49:27 +0200

    6.1. One Direction in arrays.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import './App.css';
 
 function App() {
+
+  let OneDirection = ["Liam", "Louis", "Harry", "Zayn"];
+  OneDirection.push("Niall");
+
+  let CuteOneDirection = OneDirection.map(guy => "cute " + guy);
+
+  let PeopletoPrint = [];
+
+  for (let cutePeopleIndex in OneDirection) {
+    PeopletoPrint.push(" " + CuteOneDirection[cutePeopleIndex] + ", ")
+  }
+
   return (
-    <div className="App">
-    </div>
+      <div>
+        <div>People that are cute put in an array and invoked just straight up: {OneDirection}</div>
+        <div>People that are cute put in an array converted into a string by React: {OneDirection + ""}</div>
+        <div> People that are cute put in an array and invoked with
+          appropriate breaks between them thanks to the join function:
+          &nbsp;{OneDirection.join(", ")}</div>
+        <div>People that are cute in an array with cuteness added thanks to the map function:
+          {CuteOneDirection.map((guy) =>
+              <span key={guy}> {guy}, </span>)}</div>
+        <div>Done with map function recreated with for-let-in: {PeopletoPrint}</div>
+      </div>
   );
 }
 
