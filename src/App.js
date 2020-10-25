177c1b9604245721def9dea2d5111634bce29961
Paula Krupa <paula.krupa@gmail.com>
2020-10-25 07:45:04 +0100

    7.2_alt_alt. With Math.random.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 let myFavoriteCultivators = ['Lan Xichen','Jin Guangyao','Nie Huaisang','Wei Wuxian','Jiang Cheng']
 
-let myAbsoluteFavoriteCultivators = myFavoriteCultivators.map((element, index)=><div key={"cultivator" + index}>{element}</div>)
+let myAbsoluteFavoriteCultivators = myFavoriteCultivators.map(element=><div key={"cultivator" + Math.random()}>{element}</div>)
 
 function App() {
   return (
